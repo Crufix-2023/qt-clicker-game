0d54a6a73a60df30e0aded109fcf9a7c3562b914
VaaK <155760866+Crufix-2023@users.noreply.github.com>
2026-07-31 17:32:32 +0300

    test program(work success)

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -5,13 +5,27 @@
 
 class MainWindow : public QMainWindow
 {
+    Q_OBJECT
+
+
+private:
+    Ui::MainWindow ui;
+
+    qint32 count = 0;
+
 public:
     explicit MainWindow(QWidget *parent = nullptr)
         : QMainWindow(parent)
     {
         ui.setupUi(this);
-    }
 
-private:
-    Ui::MainWindow ui;
+        ui.btn_1->setText("Жми");
+        ui.label_1->setText(QString("%0").arg(count));
+
+        connect(ui.btn_1, &QPushButton::clicked, this, [this]() {
+            count++;
+            ui.label_1->setText(QString("%1").arg(count));
+        });
+
+    }
 };
